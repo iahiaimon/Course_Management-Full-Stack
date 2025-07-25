6e6c99e6672fa48ba4b72d80134f190b8a9592ec
iahia <iahiaimon555@gmail.com>
2025-07-26 00:48:08 +0600

    Lesson Updated

--- Course Management Backend/courses/models/lesson.py
+++ Course Management Backend/courses/models/lesson.py
@@ -2,11 +2,13 @@ from django.db import models
 from core.models.basemodel import BaseModel
 
 from courses.models.course import Course
+from accounts.models import CustomUser
 
 class Lesson(BaseModel):
     title = models.CharField(max_length=255)
     description = models.TextField()
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    instructor = models.ForeignKey(CustomUser , on_delete=models.CASCADE , null=True , blank=True)
 
     def __str__(self):
         return f"{self.title} -- {self.course.title}"
