67dffc175cf800d4b4e52a037e43500dd8391d53
iahia <iahiaimon555@gmail.com>
2025-07-25 23:22:56 +0600

    Lesson Model

--- Course Management Backend/courses/models/lesson.py
+++ Course Management Backend/courses/models/lesson.py
@@ -1,4 +1,21 @@
 from django.db import models
+from core.models.basemodel import BaseModel
 
-class Lesson(models.Model):
-    pass
+from courses.models.course import Course
+
+class Lesson(BaseModel):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    course = models.ForeignKey(Course , on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"{self.title} -- {self.course.title}"
+    
+
+class LessonMaterial(BaseModel):
+    lesson_material = models.FileField(upload_to="Lesson_Material/" , blank=True , null= True ,default= None)
+    lesson = models.ForeignKey(Lesson , on_delete=models.CASCADE , related_name="lesson_materials")
+
+    def __str__(self):
+        return self.lesson_material.name if self.lesson_material else "No Material"
---
+++ Course Management Backend/courses/models/lesson_material.py
@@ -0,0 +1,5 @@
+from django.db import models
+from core.models.basemodel import BaseModel
+
+from courses.models.lesson import Lesson
+
